96deafe0b90fd90a49e56eaf0a4650be0f82b535
chamchimayo <chamchi.maayo@gmail.com>
2022-10-21 14:46:02 +0900

    first commit

---
+++ app.js
@@ -0,0 +1,19 @@
+const express = require("express");
+const app = express();
+const Http = require('http');
+const http = Http.createServer(app);
+const PORT = 3000;
+const routes = require("./routes");
+
+
+app.use(express.json());
+
+app.use("/", routes);
+
+app.get("/", (req, res) => {
+    res.send("Hello!");
+});
+
+http.listen(PORT, () => {
+    console.log(PORT, "로 연결되었습니다.");
+})
---
+++ config/config.js
@@ -0,0 +1,25 @@
+require('dotenv').config();
+
+const development = {
+'username': process.env.DB_USERNAME,
+'password': process.env.DB_PASSWORD,
+'database': process.env.DB_NAME,
+'host' : process.env.DB_END_POINT,
+'dialect' : 'mysql'
+};
+const test = {
+'username': process.env.DB_USERNAME,
+'password': process.env.DB_PASSWORD,
+'database': "mini-project-test",
+'host' : process.env.DB_END_POINT,
+'dialect' : 'mysql'
+};
+const production = {
+'username': 'root',
+'password': null,
+'database': 'database_production',
+'host' : '127.0.0.1',
+'dialect' : 'mysql'
+};
+
+module.exports = {development, test, production};
