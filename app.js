61b1142790560a70b08478d3184813156a4144b4
chamchimayo <chamchi.maayo@gmail.com>
2022-10-21 20:37:53 +0900

    백지영: 공공 API 불러오기 연습 중 10월 21일 20:37

--- app.js
+++ app.js
@@ -2,9 +2,10 @@ const express = require("express");
 const app = express();
 const Http = require('http');
 const http = Http.createServer(app);
-const PORT = 3000;
+const PORT = process.env.EXPRESS_PORT || 3000;
 const routes = require("./routes");
 
+app.use(express.static("public_html"));
 app.use(express.json());
 
 app.use("/", routes);
