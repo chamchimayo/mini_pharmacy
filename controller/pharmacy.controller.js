00b4ea2ca81f090524faf3d58c2f0b6fb6229537
DESKTOP-TGHAAU4\박ㄷ현 <bdh407@naver.com>
2022-10-22 20:57:13 +0900

    박대현: 에러 핸들러 추가 및 약국 상세조회 API 추가 10-22 오후 8시 57분

--- controller/pharmacy.controller.js
+++ controller/pharmacy.controller.js
@@ -6,18 +6,39 @@ class PhController {
         this.phService = new PhService();
     }
     getPharmacyList = async (req, res) => {        
-    const { Q0, Q1, QT, QN, ORD, pageNo, numOfRows } = req.query;
+    try {
+        const { Q0, Q1, QT, QN, ORD, pageNo, numOfRows } = req.query;
 
-    let list = await this.phService.api(Q0,  Q1, QT, QN, ORD, pageNo, numOfRows)
+        let list = await this.phService.findAll(Q0,  Q1, QT, QN, ORD, pageNo, numOfRows)
+    
+        res.setHeader("Access-Control-Allow-Origin","*")
+        res.json(list.data.response.body)
+    
+    } catch(err) {
+        next(err);
+    }   
+}
 
-    res.setHeader("Access-Control-Allow-Origin","*")
-    res.json(list.data.response.body)
+    getPharmacyOne = async(req,res) => {
+    
+        try {
+            const { pharmacyNum } = req.params;
+            
+        let pharmacy = await this.phService.findOne(pharmacyNum)
+
+        res.setHeader("Access-Control-Allow-Origin","*")
+        res.json(pharmacy.data.response.body.items)
+        } catch (err) {
+            next(err);
+        }
+              
+        }
+
+
+
 // list.then( (response) => {
 // res.setHeader("Access-Control-Allow-Origin","*")
 // res.json(response.data.response.body)   
 // })    
-}
-
-
 }
 module.exports = PhController;
